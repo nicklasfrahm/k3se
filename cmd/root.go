3e15a50948451f4520d4b0a183084aa06e795492
Nicklas Frahm <nicklas.frahm@gmail.com>
2022-04-09 20:02:25 +0200

    chore: set up cobra project

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var help bool
+
+var rootCmd = &cobra.Command{
+	Use:   "k3se",
+	Short: "CLI to manage k3s declaratively",
+	Long: `  _    _____
+ | | _|___ / ___  ___
+ | |/ / |_ \/ __|/ _ \
+ |   < ___) \__ \  __/
+ |_|\_\____/|___/\___|
+
+A lightweight Kubernetes engine that deploys k3s
+clusters declaratively based on a cluster config
+file.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if help {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+		os.Exit(0)
+	},
+	SilenceUsage: true,
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&help, "help", "h", false, "display help for command")
+}
+
+// Execute starts the invocation of the command line interface.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/nicklasfrahm/k3se/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
